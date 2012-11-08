413ad5de9156bbd0af89bc5c137f6517209292aa
Juha Muilu <juha.muilu@helsinki.fi>
2012-11-08 16:42:18 +0200

    example code added.

---
+++ src/java/varioml/src/examples/Example.java
@@ -0,0 +1,29 @@
+package examples;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.varioml.jaxb.CafeVariome;
+import org.varioml.util.Util;
+
+import scala.actors.threadpool.helpers.Utils;
+
+public class Example {
+	
+	
+	public static void main(String[] args) throws Exception {
+		Util u = new Util();
+		
+		URL url1 = new URL("https://raw.github.com/VarioML/VarioML/master/xml/cafe_variome/examples/alamut_example.xml");
+		URL url2 = new URL("https://raw.github.com/VarioML/VarioML/master/xml/cafe_variome/examples/cafe_variome_example.xml");
+		BufferedReader in = new BufferedReader( new InputStreamReader(url1.openStream()));
+		
+		CafeVariome cv1 = (CafeVariome) u.readXML("schema/cafe_variome.xsd", url1.openStream(), CafeVariome.class);
+		u.writeJSON("alamut_example.json", cv1);
+		CafeVariome cv2 = (CafeVariome) u.readXML("schema/cafe_variome.xsd", url2.openStream(), CafeVariome.class);
+		u.writeJSON("cafe_variome_example.json", cv2);
+		
+;	}
+
+}
