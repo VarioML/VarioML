1c5399cc6494324a20ccfc7f7c503264c55e51a9
Juha Muilu <juha.muilu@helsinki.fi>
2013-02-10 12:44:35 +0200

    name space prefixing enabled (default name spacw set to "v". See package-info.java class in org.varioml.jaxb)

---
+++ src/java/varioml/src/org/varioml/jaxb/package-info.java
@@ -0,0 +1,8 @@
+//@javax.xml.bind.annotation.XmlSchema(namespace = "http://varioml.org/xml/1.0"
+//, xmlns = {
+//    @XmlNs(prefix = "v", namespaceURI = "http://varioml.org/xml/1.0")
+//}
+//, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+//
+package org.varioml.jaxb;
+import javax.xml.bind.annotation.* ;
